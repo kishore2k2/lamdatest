18f7dba9751dbe9b60859929471d924cdbb4aa82
kishore2k2 <bsk26072002@gmail.com>
2026-02-03 20:22:24 +0530

    Install dotenv and update .gitignore for .env

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,35 @@
 import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 60000,
-  retries: 1,
-  fullyParallel: true,
+  timeout: 60 * 1000,
 
   use: {
-    headless: true,
-    screenshot: 'on',
-    video: 'on',
-    trace: 'on',
-  },
-
-  projects: [
-    {
-      name: 'Chromium-Windows',
-      use: { browserName: 'chromium' },
-    },
-  ],
+    connectOptions: {
+      wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+        JSON.stringify({
+          browserName: 'Chrome',
+          browserVersion: 'latest',
+          platform: 'Windows 11',
+          'LT:Options': {
+            user: process.env.LT_USERNAME,
+            accessKey: process.env.LT_ACCESS_KEY,
+            project: 'Playwright TS Hackathon',
+            build: 'GitHub TS Build',
+            name: 'LambdaTest Session',
+            network: true,
+            video: true,
+            console: true,
+            github: {
+              repo: ' kishore2k2/lamdatest', // Replace with your GitHub repo
+              branch: 'main'
+            }
+          }
+        })
+      )}`     
+    }
+  }
 });
